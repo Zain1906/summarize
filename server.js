5176375b1b7d17b2e0e3cd599930373a41319062
Zain1906 <baigmuhammadzain@gmail.com>
2025-03-26 06:36:33 +0500

    third commit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const app = express();
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'https://caseevalpro.com', // Allow requests from your frontend
+  origin: '*', // Allow requests from your frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
   credentials: true // Allow credentials (if needed)
 }));
